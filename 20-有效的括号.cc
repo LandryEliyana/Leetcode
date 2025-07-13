563011a9384f64e67c7a3f68113abbd5f9f03359
LandryEliyana <163668510+LandryEliyana@users.noreply.github.com>
2025-07-13 20:06:00 +0800

    有效的括号

---
+++ 20-有效的括号.cc
@@ -0,0 +1,83 @@
+/*
+给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串 s ，判断字符串是否有效。
+
+有效字符串需满足：
+
+左括号必须用相同类型的右括号闭合。
+左括号必须以正确的顺序闭合。
+每个右括号都有一个对应的相同类型的左括号。 
+
+示例 1：
+
+输入：s = "()"
+
+输出：true
+
+示例 2：
+
+输入：s = "()[]{}"
+
+输出：true
+
+示例 3：
+
+输入：s = "(]"
+
+输出：false
+
+示例 4：
+
+输入：s = "([])"
+
+输出：true
+
+提示：
+
+1 <= s.length <= 104
+s 仅由括号 '()[]{}' 组成
+*/
+
+#include <iostream>
+#include <stack>
+#include <unordered_map>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    bool isValid(string s) {
+        int n = s.size();
+        if (n % 2 == 1) {
+            return false;
+        }
+        unordered_map<char, char> pairs = {
+            {')', '('},
+            {']', '['},
+            {'}', '{'}
+        };
+        stack<char> stk;
+        for (int i = 0; i < n; i++) {
+            char ch = s[i];
+            if (pairs.count(ch)) {
+                if (stk.empty() || stk.top() != pairs[ch]) {
+                    return false;
+                }
+                stk.pop();
+            } else {
+                stk.push(ch);
+            }
+        }
+        return stk.empty();
+    }
+};
+
+int main() {
+    Solution solution;
+    string s;
+    cout << "please input a string: " << endl;
+    cin >> s;
+    bool result = solution.isValid(s);
+    cout << "Input: s = " << s << endl;
+    cout << "Output: " << boolalpha << result << endl;
+    return 0;
+}
